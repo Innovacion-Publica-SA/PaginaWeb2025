38d3580e43d402dac1e332bdf89be9512704bfb9
José Mariano Reyes <josemarianoreyesh02@gmail.com>
2024-10-25 10:12:59 -0600

    new home

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,47 @@
+import Blog4 from "@/components/sections/Blog4"
+import Cta4 from "@/components/sections/Cta4"
+import Cta5 from "@/components/sections/Cta5"
+import Cta6 from "@/components/sections/Cta6"
+import Features8 from "@/components/sections/Features8"
+import Features9 from "@/components/sections/Features9"
+import Hero4 from "@/components/sections/Hero4"
+import HowItWork2 from "@/components/sections/HowItWork2"
+import LogoCloud4 from "@/components/sections/LogoCloud4"
+import Newsletter3 from "@/components/sections/Newsletter3"
+import Services3 from "@/components/sections/Services3"
+import Static3 from "@/components/sections/Static3"
+import Team3 from "@/components/sections/Team3"
+import dynamic from 'next/dynamic'
+
+const Services5 = dynamic(() => import('@/components/sections/Services5'), {
+	ssr: false,
+})
+
 import Layout from "@/components/layout/Layout"
-import Blog1 from "@/components/sections/Blog1"
-import Features1 from "@/components/sections/Features1"
-import Features2 from "@/components/sections/Features2"
-import Features3 from "@/components/sections/Features3"
-import Hero1 from "@/components/sections/Hero1"
-import LogoCloud1 from "@/components/sections/LogoCloud1"
-import Newsletter1 from "@/components/sections/Newsletter1"
-import Pricing1 from "@/components/sections/Pricing1"
-import Services1 from "@/components/sections/Services1"
-import Team1 from "@/components/sections/Team1"
-import Testimonial1 from "@/components/sections/Testimonial1"
 export default function Home() {
 
 	return (
 		<>
+			<Layout headerStyle={4} footerStyle={3}>
 
-			<Layout headerStyle={1} footerStyle={1}>
-				<Hero1 />
-				<LogoCloud1 />
-				<Features1 />
-				<Features2 />
-				<Features3 />
-				<Services1 />
-				<Team1 />
-				<Pricing1 />
-				<Testimonial1 />
-				<Blog1 />
-				<Newsletter1 />
+				<Hero4 />
+				<LogoCloud4 />
+				<Services3 />
+				<Cta4 />
+				<Static3 />
+				<Team3 />
+				<Services5 />
+				<Features8 />
+				<Features9 />
+				<Cta5 />
+				<HowItWork2 />
+				<Cta6 />
+				<Blog4 />
+				<Newsletter3 />
 
 			</Layout>
+
+			
 		</>
 	)
 }
